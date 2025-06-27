827f78a4349aa98c3e0de8cec637e66b155cd231
enayetsyl <enayetflweb@gmail.com>
2025-06-27 22:38:36 +0600

    backend build file push

---
+++ server/dist/app.js
@@ -0,0 +1,16 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const cors_1 = __importDefault(require("cors"));
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use((0, cookie_parser_1.default)());
+app.use((0, cors_1.default)({ origin: ['http://localhost:5173', 'http://localhost:3000', 'https://halal-biye.vercel.app'], credentials: true }));
+app.get('/', (req, res) => {
+    res.send('Hello from Halal Biye Server');
+});
+exports.default = app;
