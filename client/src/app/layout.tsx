e86a7ac3346e2e9a5f2afb9b44e7c47d538c19c1
enayetsyl <enayetflweb@gmail.com>
2025-06-27 22:01:06 +0600

    shadcn ui component install done

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "HalalBiye",
+  description:
+    "HalalBiye is an elegant, secure matrimonial platform for Muslim singles. Create detailed profiles, browse and filter matches by faith and values, and send connection requests for your halal marriage journey.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+       <main>{children}</main>
+        <Toaster
+        position="bottom-right"
+richColors
+
+        />
+      </body>
+    </html>
+  );
+}
